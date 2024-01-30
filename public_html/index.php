20e53a6a9da05c059234ecfe62e014e861380442
Frederik Ring <frederik.ring@wikimedia.de>
2024-01-30 11:47:46 +0100

    fix: make sure all cookies expire at the same point in time

--- public_html/index.php
+++ public_html/index.php
@@ -33,7 +33,9 @@ header('Access-Control-Allow-Origin: https://petscan-dev.wmflabs.org');
 */
 
 require_once ( 'php/Widar.php' ) ;
-$widar = new Widar ( 'widar' ) ;
+$widar = new Widar ( 'widar', function () {
+    return time()+60*60*24*30*3; // make all cookies expire in three months
+} ) ;
 if ( !$widar->render_reponse ( $botmode ) ) $widar->output_widar_main_page () ;
 
 ?>
